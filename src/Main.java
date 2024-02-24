b98b816103856b09f30e3961010a7f00f353154d
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-02-24 13:06:30 +0530

    calling sleep for thread

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,19 @@ public class Main {
         Runnable runnable = () -> {
             String threadName = Thread.currentThread().getName();
             System.out.println(threadName + " running");
+
+            try {
+                Thread.sleep(2000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+            System.out.println(threadName + " finished");
         };
 
         Thread thread1 = new Thread(runnable, "The Thread 1");
         thread1.start();
 
-        Thread thread2 = new Thread(runnable, "The Thread 2");
-        thread2.start();
 
     }
 }
