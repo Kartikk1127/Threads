0d5f0e83a45d7c9615ddc630ab3b1fef9080ce44
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-02-24 13:21:16 +0530

    stopping a thread

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,52 @@
 public class Main {
-    public static void main(String[] args) {
 
-        Runnable runnable = () -> {
-            String threadName = Thread.currentThread().getName();
-            System.out.println(threadName + " running");
+    public static class StoppableRunnable implements Runnable{
 
+        private boolean stopRequested = false;
+
+        public synchronized void requestStop(){
+            this.stopRequested = true;
+        }
+
+        public synchronized boolean isStopRequested() {
+            return this.stopRequested;
+        }
+
+        public void sleep(long millis){
             try {
-                Thread.sleep(2000);
-            }catch (InterruptedException e){
+                Thread.sleep(millis);
+            } catch (InterruptedException e){
                 e.printStackTrace();
             }
+        }
+        @Override
+        public void run() {
+            System.out.println("StoppableRunnable running");
 
-            System.out.println(threadName + " finished");
-        };
+            while (!isStopRequested()){
+                sleep(1000);
+                System.out.println("...");
+            }
 
-        Thread thread1 = new Thread(runnable, "The Thread 1");
+            System.out.println("StoppableRunnable finished");
+
+        }
+    }
+    public static void main(String[] args) {
+
+        StoppableRunnable runnable = new StoppableRunnable();
+        Thread thread1 = new Thread(runnable, "The Thread ");
         thread1.start();
+
+        try {
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("requesting stop");
+        runnable.requestStop();
+        System.out.println("stop requested");
 
 
     }
