0054779f25dd0a907990ec4f63698a02aa2fcb5b
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-02-24 12:42:28 +0530

    implement an anonymous class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
-
-    public static class MyRunnable implements Runnable {
-
-        @Override
-        public void run() {
-            System.out.println("My Runnable thread started");
-            System.out.println("My Runnable thread finished");
-        }
-    }
     public static void main(String[] args) {
 
-        Thread thread = new Thread(new MyRunnable());
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("runnable running");
+                System.out.println("runnable finished");
+            }
+        };
+
+        Thread thread = new Thread(runnable);
 
         thread.start();
+
     }
 }
