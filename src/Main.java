e9ed26540c92311cc751cce07b090833f837c97d
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-02-29 06:14:17 +0530

    race condition example

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,9 @@ public class Main {
     public static void main(String[] args) {
 
         MyRunnable runnable1 = new MyRunnable();
-        MyRunnable runnable2 = new MyRunnable();
 
         Thread thread1 = new Thread(runnable1);
-        Thread thread2 = new Thread(runnable2);
+        Thread thread2 = new Thread(runnable1);
 
         thread1.start();
         thread2.start();
