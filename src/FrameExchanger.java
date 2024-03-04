8461ec997db14f332fcc00b83d5317ce4ff2c5c8
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-03-04 05:56:29 +0530

    use case of volatile keyword

--- src/FrameExchanger.java
+++ src/FrameExchanger.java
@@ -4,7 +4,7 @@ when the hasNewFrame variable is set to true, the frame and frameStoredCount
 will also be synchronized to main memory. Additionally, every time the drawing
 thread reads the hasNewFrame variable in the while-loop inside the takeFrame() method,
 the frame and framesStoredCount will also be refreshed from main memory.
-Even framesTakenCount will get updated from main memory at this point.
+Even the framesTakenCount will get updated from main memory at this point.
 *  */
 
 public class FrameExchanger {
