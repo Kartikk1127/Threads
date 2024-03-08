0ae3ac333ed7e169a574a785d50dafd0f1d3cf40
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-03-08 05:58:53 +0530

    synchtonized block in lambda expression

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,34 @@
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
 
-    public static class MyRunnable implements Runnable{
-
-        private int count = 0;
-
-        @Override
-        public void run() {
-
-            for(int i=0;i<1000000;i++){
-                this.count++;
-            }
-            System.out.println(Thread.currentThread().getName()+": "+count);
-        }
-    }
-
     public static void main(String[] args) {
 
-        MyRunnable runnable1 = new MyRunnable();
+        Consumer<String> func = (String param) -> {
+          synchronized (SynchronizedExample.class){
+              System.out.println(Thread.currentThread().getName() +
+                      " step 1: " + param);
 
-        Thread thread1 = new Thread(runnable1);
-        Thread thread2 = new Thread(runnable1);
+              try {
+                  Thread.sleep((long) (Math.random() * 1000));
+              } catch (InterruptedException e){
+                  e.printStackTrace();
+              }
+              System.out.println(Thread.currentThread().getName() +
+                      " step 2: " + param);
+          }
+        };
+
+        Thread thread1 = new Thread(()->{
+           func.accept("Parameter");
+        }, "Thread 1");
+
+        Thread thread2 = new Thread(()->{
+            func.accept("Parameter");
+        },"Thread 2");
 
         thread1.start();
         thread2.start();
---
+++ src/SynchronizedExample.java
@@ -0,0 +1,2 @@
+public class SynchronizedExample {
+}
